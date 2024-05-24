9df3f0b80e9feb7b2be77bc4c0d15b813f0b5505
Aaron Edwards <aaron@uglyrobot.com>
2024-05-24 18:02:03 -0500

    Upgrade and rework for our API

--- src/index.ts
+++ src/index.ts
@@ -13,21 +13,32 @@ export default {
     const { searchParams } = new URL(request.url)
     let url = searchParams.get('url')
     let img: Buffer
+    let thumbnail: Buffer
     if (url) {
       const browser = await puppeteer.launch(env.mybrowser)
       const page = await browser.newPage()
+      await page.setViewport({ width: 1280, height: 960 })
       await page.goto(url)
-      img = (await page.screenshot({ fullPage: true })) as Buffer
-      const title = sanitize(await page.title(), { replacement: '-' })
+      await page.waitForFunction('document.readyState === "complete"')
+      img = (await page.screenshot({ fullPage: true, type: 'jpeg', quality: 80 })) as Buffer
+      const title = sanitize(await page.title(), { replacement: '-' }).replace(/\s+/g, '-')
       const id = nanoid(5)
 
       const titleString = `${title}_${id}.jpg`
+      thumbnail = (await page.screenshot({ clip: { x: 0, y: 0, width: 1280, height: 960 }, type: 'jpeg', quality: 80 })) as Buffer
+      const thumbnailTitle = `thumbnail_${title}_${id}.jpg`
 
       await browser.close()
       await env.puppeteer.put(titleString, img)
-      return Response.redirect(`${env.bucketUrl}/${titleString}`, 307)
+      await env.puppeteer.put(thumbnailTitle, thumbnail)
+      return new Response(JSON.stringify({ full: `${env.bucketUrl}/${titleString}`, thumbnail: `${env.bucketUrl}/${thumbnailTitle}` }), {
+        headers: { 'Content-Type': 'application/json' },
+      })
     } else {
-      return new Response('Please add the ?url=https://example.com/ parameter')
+      return new Response(JSON.stringify({ error: 'Please add the ?url=https://example.com/ parameter' }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' },
+      })
     }
   },
 }
